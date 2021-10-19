9e380f11040b5982660bbc39dc95704ccf231b78
Michal J. Gasior <michaljgasior@gmail.com>
2021-10-19 21:16:04 +0200

    Initialized GraphQL

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() {
   runApp(const CrackerApp());
 }
 
-class CrackerApp extends StatelessWidget {
+class CrackerApp extends StatefulWidget {
   const CrackerApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<CrackerApp> createState() => _CrackerAppState();
+}
+
+class _CrackerAppState extends State<CrackerApp> {
+  static const $API_ADDRESS = 'https://cracker.red/api';
+  ValueNotifier<GraphQLClient>? client;
+
+  void initializeClient() {
+    final HttpLink httpLink = HttpLink($API_ADDRESS);
+    ValueNotifier<GraphQLClient> newClient = ValueNotifier(GraphQLClient(
+      link: httpLink,
+      cache: GraphQLCache(store: InMemoryStore()),
+    ));
+
+    setState(() {
+      client = newClient;
+    });
+  }
+
+
+  @override
+  void initState() {
+    initializeClient();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    if (client == null) {
+      return const MaterialApp(home: Text("Loading..."));
+    }
+
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        title: 'Cracker mobile app',
+        home: const Text('Flutter Demo Home Page'),
       ),
-      home: const Text('Flutter Demo Home Page'),
     );
   }
 }
