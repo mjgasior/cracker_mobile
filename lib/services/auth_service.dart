175475695515eb7686d1e8e788ab30ced4ae5420
Michal J. Gasior <michaljgasior@gmail.com>
2021-10-22 09:42:21 +0200

    Auth service login method

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import '../models/auth0_user.dart';
+import '../models/auth0_id_token.dart';
 import '../helpers/constants.dart';
-// import 'package:mjcoffee/models/auth0_id_token.dart';
-// import 'package:mjcoffee/models/auth0_user.dart';
 
 class AuthService {
   static final AuthService instance = AuthService._internal();
@@ -16,19 +16,86 @@ class AuthService {
   final FlutterAppAuth appAuth = FlutterAppAuth();
   final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
 
+  Auth0User? profile;
+  Auth0IdToken? idToken;
+  String? auth0AccessToken;
+
   login() async {
-    final authorizationTokenRequest = AuthorizationTokenRequest(
-      auth0ClientId,
-      auth0RedirectUri,
-      issuer: auth0Issuer,
-      scopes: ['openid', 'profile', 'offline_access', 'email'],
+    try {
+      final authorizationTokenRequest = AuthorizationTokenRequest(
+        auth0ClientId,
+        auth0RedirectUri,
+        issuer: auth0Issuer,
+        scopes: ['openid', 'profile', 'offline_access', 'email'],
+      );
+
+      final AuthorizationTokenResponse? result =
+          await appAuth.authorizeAndExchangeCode(
+        authorizationTokenRequest,
+      );
+
+      return await _setLocalVariables(result);
+    } on PlatformException {
+      return 'User has cancelled or no Internet!';
+    } catch (e) {
+      return 'Unknown error!';
+    }
+  }
+
+  Auth0IdToken parseIdToken(String idToken) {
+    final parts = idToken.split(r'.');
+    assert(parts.length == 3);
+
+    final Map<String, dynamic> json = jsonDecode(
+      utf8.decode(
+        base64Url.decode(
+          base64Url.normalize(parts[1]),
+        ),
+      ),
     );
 
-    final AuthorizationTokenResponse? result =
-        await appAuth.authorizeAndExchangeCode(
-      authorizationTokenRequest,
+    return Auth0IdToken.fromJson(json);
+  }
+
+  Future<Auth0User> getUserDetails(String accessToken) async {
+    final url = Uri.https(
+      auth0Domain,
+      '/userinfo',
     );
 
-    print(result);
+    final response = await http.get(
+      url,
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+
+    print('getUserDetails ${response.body}');
+
+    if (response.statusCode == 200) {
+      return Auth0User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to get user details');
+    }
+  }
+
+  Future<String> _setLocalVariables(result) async {
+    final bool isValidResult =
+        result != null && result.accessToken != null && result.idToken != null;
+
+    if (isValidResult) {
+      auth0AccessToken = result.accessToken;
+      idToken = parseIdToken(result.idToken!);
+      profile = await getUserDetails(result.accessToken!);
+
+      if (result.refreshToken != null) {
+        await secureStorage.write(
+          key: REFRESH_TOKEN_KEY,
+          value: result.refreshToken,
+        );
+      }
+
+      return 'Success';
+    } else {
+      return 'Something is wrong!';
+    }
   }
 }
